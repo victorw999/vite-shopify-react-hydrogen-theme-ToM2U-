c22a8ce29816b69012f7f471d66cc36626ecf2e1
Victor Wong <=>
2024-04-17 13:23:20 -0700

    4/17/2024

--- frontend/entrypoints/react/routes/index.jsx
+++ frontend/entrypoints/react/routes/index.jsx
@@ -1,10 +1,7 @@
 export default function Index() {
   return (
     <p id="zero-state" className=" bg-transparent">
-      This is a demo for React Router.
-      <br />
-      Check out{' '}
-      <a href="https://reactrouter.com">the docs at reactrouter.com</a>.
+ 
     </p>
   )
 }
