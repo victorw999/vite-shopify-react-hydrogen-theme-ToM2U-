048653999fa6c585b47eaac9e5aa35543da37f3c
Victor Wong <=>
2024-09-04 15:02:34 -0700

    modify customers-data app-proxy explainer txt,

--- frontend/entrypoints/react/features/customers/CustomerIndex.jsx
+++ frontend/entrypoints/react/features/customers/CustomerIndex.jsx
@@ -10,7 +10,7 @@ import { Navigate } from "react-router-dom";
 export default function CustomerIndex() {
   const { customers } = useSelector(state => state.customers)
 
-  // redirection logic: 
+  // redirection logic:  
   // if (customers.length > 0) {
   //   // strip the gid's prefix to get the customer id
   //   // first customer's id
@@ -20,8 +20,9 @@ export default function CustomerIndex() {
 
   return (
     <div className="customerIndex section_index_page bg-transparent">
-      <h2 className="section_heading_normalcase">
-        <b>Purchase History:</b> Utilize Shopify's  <a href="https://shopify.dev/docs/api/admin-graphql"> GraphQL Admin API </a> to extract detailed customer data for business intelligence. This includes metrics like total spending since account creation. By accessing this data, businesses can gain insights into customer behavior, optimize marketing, and improve customer relationship management, driving growth and success.
+      <h2 className="section_heading_normalcase"> 
+        <b>Purchase History:</b> To securely fetch data that's accessible exclusively via the admin API without exposing the API key, we must create a custom app utilizing <a href="https://shopify.dev/docs/api/shopify-app-remix/v2/authenticate/public/app-proxy"> Shopify's App Proxy</a>. This approach involves the frontend making the data requests to an Remix endpoint, reserved for the proxy, which then securely calls the <a href="https://shopify.dev/docs/api/admin-graphql"> GraphQL admin API </a> on Shopify server. This intermediary layer effectively <a href="https://shopify.dev/docs/apps/build/authentication-authorization/access-tokens/generate-app-access-tokens-admin">safeguards</a> the admin API key.
+
       </h2>
     </div>
   )
