b303a577ea88609b1b3c6a63b5bdfedc4a5ba675
Victor Wong <=>
2025-04-01 08:23:08 -0700

    fix lint errs notifications

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,12 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ['standard', 'plugin:prettier/recommended']
+  extends: ['standard', 'plugin:prettier/recommended'],
+  rules: {
+    'prettier/prettier': ['error', {
+      endOfLine: 'auto',
+      semi: false,
+      singleQuote: true,
+      trailingComma: 'none'
+    }]
+  }
 }
